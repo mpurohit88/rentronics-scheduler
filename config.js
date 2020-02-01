aeec73f439d5fd374db9a22e8f71b6061bb68d6a
root <root@vps.saimrc.com>
2020-02-01 09:29:45 +0530

    merge comflict

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const env = 'dev';
 
 let domainName;
-let mailPass = '';
+let mailPass = 'y&GFhE16U';
 let mailService = 'rentronics.saimrc.com'
 
 if (env === 'uat') {
@@ -10,7 +10,7 @@ if (env === 'uat') {
 } else if (env === 'prod') {
   domainName = 'rentronics.a1abilities.co.nz';
   mailService = 'rentronics.a1abilities.co.nz';
-  mailPass = '';
+  mailPass = 'y&GFh$16U';
 } else if (env === 'dev') {
   domainName = 'rentronicsdev.saimrc.com'
 } else {
